1648a27c40c54077728004df52ff77648f428c8f
André Veríssimo <afsverissimo@gmail.com>
2020-04-03 02:55:41 +0200

    won't display paged tables anymore

---
+++ R/data.R
@@ -0,0 +1,17 @@
+#' PT DGS and EU CDC data combined
+#'
+#' @name covid19.pt
+#' @docType data
+#' @keywords data
+#' @examples
+#' head(covid19.pt)
+'covid19.pt'
+
+#' PT DGS data
+#'
+#' @name dgs.pt
+#' @docType data
+#' @keywords data
+#' @examples
+#' head(dgs.pt)
+'dgs.pt'
