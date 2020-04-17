55938e6fda54c39575db97ae205e10579e29a679
André Veríssimo <afsverissimo@gmail.com>
2020-04-18 01:36:09 +0200

    improves age plot showing mortality rate

---
+++ R/plots.R
@@ -0,0 +1,33 @@
+build.labels <- function(input, order.by, name, digits = 2) {
+  my.format <- function(val) {
+    val %>%
+      round(digits = digits) %>%
+      format(big.mark = ',', trim = TRUE) %>%
+      return()
+  }
+
+  tmp <- input %>%
+    dplyr::arrange(desc(date), desc(!!as.name(order.by))) %>%
+    dplyr::mutate(state.data = !!as.name(name),
+                  state.data.order = !!as.name(order.by),
+                  state.data.val.max = max(!!as.name(order.by), na.rm = TRUE),
+                  state.data.val.last = first(!!as.name(order.by), default = 0)) %>%
+    dplyr::mutate(state.data.label = paste0(state.data,
+                                            ' (today: ',
+                                            state.data.val.last %>% my.format,
+                                            ' max: ',
+                                            max(!!as.name(order.by), na.rm = TRUE) %>% my.format,
+                                            ' min: ',
+                                            min(!!as.name(order.by), na.rm = TRUE) %>% my.format,
+                                            ')'))
+
+  tmp %>%
+    dplyr::mutate(label = dplyr::if_else(state.data.order == max(state.data.order, na.rm = TRUE), state.data.label, NA_character_),
+                  state.data = factor(state.data,
+                                      levels = .$state.data %>% unique)) %>%
+    dplyr::arrange(label, desc(date)) %>%
+    dplyr::mutate(label = if_else(date == first(date), label, NA_character_)) %>%
+    dplyr::select(-state.data, -state.data.label, -state.data.val.last, -state.data.val.max, -state.data.order) %>%
+    dplyr::arrange(desc(date)) %>%
+    return()
+}
