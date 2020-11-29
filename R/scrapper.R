0272a1a44044dc9006e66a9cb7b7bab2c4addf39
André Veríssimo <afsverissimo@gmail.com>
2020-11-29 19:27:24 +0100

    goes back to previous version

--- R/scrapper.R
+++ R/scrapper.R
@@ -243,7 +243,7 @@ extract_info2 <- function(only.date = NULL, index = 1) {
                              extract_hospitalized2(page1))
 
     if (ages %>% pull('date') %>%  purrr::pluck(1) == report.pdf$date) {
-      info <- rows_update(info, ages, by = 'date')
+      info <- bind_cols(info, ages)
     }
 
   }
