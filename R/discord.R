85c35fa2277e1baa6e64268ce7e47ffe60c7c1ef
André Veríssimo <afsverissimo@gmail.com>
2021-07-03 20:01:31 +0200

    corrects a problem

--- R/discord.R
+++ R/discord.R
@@ -12,13 +12,14 @@
 #' dat2[1, 3] <- 900000
 #' send.discord.msg(dat2, dat1)
 send.discord.msg <- function(new.dat, old.dat) {
-  tryCatch({
-    webhook.env <- Sys.getenv('DISCORD_WEBHOOK')
-    if (webhook.env != "") {
-      warning("Found Discord webhook!")
-      cat("Found Discord webhook!")
-      futile.logger::flog.info("", Sys.getenv(), capture = TRUE)
 
+  webhook.env <- Sys.getenv('DISCORD_WEBHOOK')
+  if (webhook.env != "") {
+    message("Found Discord webhook!")
+    cat("Found Discord webhook!")
+    futile.logger::flog.info("", Sys.getenv(), capture = TRUE)
+
+    tryCatch({
       webhook <- strsplit(webhook.env, ';')
 
       line1.new <- new.dat %>% dplyr::top_n(1, date) %>% dplyr::select(-country)
@@ -26,7 +27,7 @@ send.discord.msg <- function(new.dat, old.dat) {
 
       msg <- c()
       if (line1.new %>% dplyr::pull(date) != line1.old %>% dplyr::pull(date)) {
-        msg.header <- c(glue::glue("**PT.COVID -- New date**: {line1.new %>% dplyr::pull(date))"))
+        msg.header <- c(glue::glue("**PT.COVID -- New data**: {line1.new %>% dplyr::pull(date))"))
       } else {
         msg.header <- c("**PT.COVID -- Data changed**")
       }
@@ -34,10 +35,12 @@ send.discord.msg <- function(new.dat, old.dat) {
       for (ix.col in colnames(line1.old  %>% dplyr::select(-date))) {
         cell.old <- line1.old[1, ix.col] %>% purrr::pluck(1)
         cell.new <- line1.new[1, ix.col] %>% purrr::pluck(1)
+
         if ((is.na(cell.old) && !is.na(cell.new)) || (!is.na(cell.new) && cell.old != cell.new)) {
           cell.new <- format(cell.new, big.mark = ' ')
           if (!is.na(cell.new) && cell.old != cell.new) {
-            msg <- c(msg, glue::glue(' * {ix.col}: {cell.new} {cell.new - cell.old}'))
+            cell.diff <- cell.new - cell.old
+            msg <- c(msg, glue::glue(' * {ix.col}: {cell.new} ({cell.diff})'))
           } else {
             msg <- c(msg, glue::glue(' * {ix.col}: {cell.new}'))
           }
@@ -57,10 +60,10 @@ send.discord.msg <- function(new.dat, old.dat) {
         )
 
       }
-    } else {
-      warning("Discord webhook is not defined. Discord msg not sent.")
-      cat("Discord webhook is not defined. Discord msg not sent.")
-    }
-  }, error = function(err) { warning("Failed at sending message to discord", err)})
+    }, error = function(err) { warning("Failed at sending message to discord", err)})
+  } else {
+    warning("Discord webhook is not defined. Discord msg not sent.")
+    cat("Discord webhook is not defined. Discord msg not sent.")
+  }
   # else do nothing
 }
